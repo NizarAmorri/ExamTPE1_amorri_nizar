2a1d002aadb4b992fa1aa5bf3bc6790c7e69a1bb
Nizar Amorri <nizar.amorri.24@gmail.com>
2025-03-14 09:38:47 +0100

    Fusion de la branche casee1

--- case.py
+++ case.py
@@ -6,6 +6,15 @@ def somme(T):
     return S
 =======
 Data=[1,3,5]
+<<<<<<< HEAD
 Som=sum(Data)
 print('la somme est :', Som)
+>>>>>>> casee1
+=======
+if Data:
+    print('la somme est :', sum(Data))
+    print('le min est :', min(Data))
+    print('le max est :', max(Data))
+else:
+    print('dossier vide')
 >>>>>>> casee1
